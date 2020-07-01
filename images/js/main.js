013d8dca3ea69f2c0d8d05e7d03a3405da953f30
DOSTON <998911585852d@gmail.com>
2020-07-01 10:09:42 +0500

    calc + Todo List
    calc + Todo List

--- images/js/main.js
+++ images/js/main.js
@@ -2,10 +2,15 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		massage: 'Hello world',
+		stylesCSS: '',
 		result: '',
 		numbers: [1,2,3,4,5,6,7,8,9,0],
 		operations: ['+','-','*','/'],
-		stylesCSS: ''
+		items: [
+			{title: 'Test1', desc: 'Test1'},
+			{title: 'Test2', desc: 'Test2'},
+			{title: 'Test3', desc: 'Test3'}
+		]
 	},
 	methods: {
 		changeText () {
@@ -18,8 +23,43 @@ var app = new Vue({
 		reset: function() {
 			this.result = '';
 		},
-		calc: function() {
+		answer: function() {
 			this.result = eval(this.result);
+		}
+	}
+});
+var app2 = new Vue({
+	el: '#app2',
+	data: {
+		toggle: '',
+		check:'',			
+		items: [
+			{title: 'Test1', desc: 'Test1'},
+			{title: 'Test2', desc: 'Test2'},
+			{title: 'Test3', desc: 'Test3'}
+		]
+	},
+	methods: {
+		addItem: function() {
+			var input = document.getElementById('listsTitle');
+			var input2 = document.getElementById('listsDesc');
+			if(input.value !== '') {
+				this.items.push({
+					title:input.value,
+					desc:input2.value
+				})
+				input.value = "";
+				input2.value = "";
+			}
+		},
+		deleteItem: function(index) {
+			this.items.splice(index, 1);
+		},
+		checkfunction: function() {
+			var checkbox = document.getElementByClass('checkbox');
+			if(checkbox.value == true) {
+				this.check = 'check'
+			} 
 		}
 	}
 })
